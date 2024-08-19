078990c148a24309f68f4d61bd2b4764c0a123e1
lenam2509 <levietnam2509@gmail.com>
2024-08-19 14:34:58 +0700

    refactor: Update layout and styles for sidebar and rightbar components

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -10,10 +10,13 @@ export default function Home() {
         <div className="text-2xl font-bold">
           GoMeal<span className="font-bold text-yellow-400">.</span>
         </div>
-        <Avatar className="cursor-pointer">
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+        <div className="flex items-center gap-4">
+          <BurgerMenu />
+          <Avatar className="cursor-pointer">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
       </div>
       <div className="flex justify-center md:justify-between">
         <h1 className="hidden text-2xl font-bold md:block">Hello, Patricia</h1>
@@ -92,6 +95,23 @@ export const SearchIcon = ({ className }: { className: string }) => (
       strokeLinecap="round"
       strokeLinejoin="round"
       d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+    />
+  </svg>
+);
+
+export const BurgerMenu = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
     />
   </svg>
 );
